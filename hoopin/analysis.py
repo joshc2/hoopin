a312144a130a5e88acc461f6e22860c2dddb4585
Griffen Coburn <griffencoburn@gmail.com>
2023-12-08 13:22:33 -0700

    edit readme

--- hoopin/analysis.py
+++ hoopin/analysis.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 
-#ALL COLUMNS
+#ALL COLUMNS 
 #all_columns = ['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', "MOV", "Pace", "TS%", 'W', 'L']
 
 # COLUMNS WE ARE KEEPING
