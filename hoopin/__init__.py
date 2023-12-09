2323cfcf7b866b71cc7f8de3857f1df2dcbef783
Joshua Cabrera <cabrerajoshua25@gmail.com>
2023-12-09 16:44:22 -0700

    visualization.py changes

--- hoopin/__init__.py
+++ hoopin/__init__.py
@@ -1,2 +1,24 @@
 from hoopin.load_data import load_data
 from hoopin.analysis import run_regression
+
+
+from hoopin.visualization import show_residuals_distribution
+
+from hoopin.visualization import show_actual_predicted
+from hoopin.visualization import show_barchart
+from hoopin.visualization import show_correlation_heatmap
+from hoopin.visualization import show_pairplot
+from hoopin.visualization import show_residuals
+# from hoopin.visualization import show_MSE
+# from hoopin.visualization import show_r2
+
+
+
+# from hoopin.load_data import load_data
+# from .hoopin.load_data import load_data2
+
+# from hoopin.visualization import show_heatmap
+# from hoopin.visualization import show_visualizations
+# from hoopin.analysis import select_columns
+# import pandas as pd
+# import seaborn as sns
--- hoopin/visualization.py
+++ hoopin/visualization.py
@@ -5,14 +5,34 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 
+# import hoopin
 # Assuming you have already loaded your data and defined the variables as mentioned in your previous code
 
 
-def show_visualizations():
+all_data_1 =  pd.read_csv("/datasets/basketball.csv")
+selected_columns = ['3P%','2P%', 'AST','TRB','STL',"TS%", 'W', 'L']
 
+
+# Split the data into predictor variables (X) and target variable (Y)
+X = all_data_1[selected_columns[:-2]]  # All columns except 'W' and 'L'
+Y = all_data_1['W']
+
+# Split the data into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# Create a linear regression model, fit it to the training data, and make predictions
+model = LinearRegression()
+model.fit(X_train, Y_train)
+Y_pred = model.predict(X_test)
+
+
+
+coefficients = pd.Series(model.coef_, index=X.columns)
+sorted_coefficients = coefficients.abs().sort_values(ascending=False)
+def show_actual_predicted():
     """
     See the data
-    """
+    """   
     # Visualization 1: Scatter plot of Actual vs Predicted Wins
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x=Y_test, y=Y_pred)
@@ -20,7 +40,8 @@ def show_visualizations():
     plt.xlabel('Actual Wins')
     plt.ylabel('Predicted Wins')
     plt.show()
-
+    return    
+def show_barchart():
     # Visualization 2: Bar chart of Coefficients
     coefficients = coefficients.sort_values(ascending=True)
     coefficients = pd.Series(model.coef_, index=X.columns)
@@ -32,14 +53,20 @@ def show_visualizations():
     plt.xlabel('Coefficient Value')
     plt.ylabel('Predictor Variables')
     plt.show()
+    return
 
     # Additional Evaluation Metrics
-    mse = mean_squared_error(Y_test, Y_pred)
-    r2 = r2_score(Y_test, Y_pred)
+# def show_MSE():
+#     mse = mean_squared_error(Y_test, Y_pred)
+#     print("R-squared:", r2)
+#     return
 
-    print("Mean Squared Error:", mse)
-    print("R-squared:", r2)
+# def show_r2():
+#     r2 = r2_score(Y_test, Y_pred)
+#     print("Mean Squared Error:", mse)
+#     return
 
+def show_residuals():
     # RESIDUAL PLOT
     plt.figure(figsize=(10, 6))
     sns.residplot(x=Y_pred, y=Y_test, lowess=True, color="g")
@@ -47,6 +74,10 @@ def show_visualizations():
     plt.xlabel('Predicted Wins')
     plt.ylabel('Residuals')
     plt.show()
+    return
+
+
+def show_residuals_distribution():
 
     # distribution plot of residuals
     plt.figure(figsize=(10, 6))
@@ -55,20 +86,23 @@ def show_visualizations():
     plt.xlabel('Residuals')
     plt.ylabel('Frequency')
     plt.show()
+    return
 
-
+def show_pairplot():
     #Pair PLOT
     sns.pairplot(all_data_1[selected_columns])
     plt.suptitle('Pair Plot of Selected Columns', y=1.02)
     plt.show()
+    return
 
+def show_correlation_heatmap():
     # CORRELATION HEAT MAP
     plt.figure(figsize=(10, 8))
     sns.heatmap(all_data_1[selected_columns].corr(), annot=True, cmap='coolwarm', fmt=".2f")
     plt.title('Correlation Heatmap of Selected Columns')
     plt.show()
-
     return
+    
 
 
 
