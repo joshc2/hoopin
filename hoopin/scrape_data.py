888c5178a381546b924e420a68c131a37534243f
Joshua Cabrera <cabrerajoshua25@gmail.com>
2023-12-09 18:37:08 -0700

    m

--- hoopin/scrape_data.py
+++ hoopin/scrape_data.py
@@ -10,96 +10,96 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
+def scrape_data():
+    # SEASON 2022-23
+    bib_url_2023 = 'https://www.basketball-reference.com/leagues/NBA_2023.html'
+    requests.get(bib_url_2023)
+    data_2023=pd.read_html(bib_url_2023)[5]
+    data2_2023=pd.read_html(bib_url_2023)[10]
+    df = pd.DataFrame(data_2023)
+    data2_2023.columns = data2_2023.columns.droplevel(0)
+    merged_data_2023 = data_2023.merge(data2_2023[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2023['Team'] = merged_data_2023['Team'].str.replace('*', '', regex=False)
+    merged_data_2023
+    merged_data_2023['Year'] = 2023
+    # Drop the last row from merged_data
+    merged_data_2023 = merged_data_2023.drop(merged_data_2023.index[-1])
 
-# SEASON 2022-23
-bib_url_2023 = 'https://www.basketball-reference.com/leagues/NBA_2023.html'
-requests.get(bib_url_2023)
-data_2023=pd.read_html(bib_url_2023)[5]
-data2_2023=pd.read_html(bib_url_2023)[10]
-df = pd.DataFrame(data_2023)
-data2_2023.columns = data2_2023.columns.droplevel(0)
-merged_data_2023 = data_2023.merge(data2_2023[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2023['Team'] = merged_data_2023['Team'].str.replace('*', '', regex=False)
-merged_data_2023
-merged_data_2023['Year'] = 2023
-# Drop the last row from merged_data
-merged_data_2023 = merged_data_2023.drop(merged_data_2023.index[-1])
+    # SEASON 2021-22
+    bib_url_2022 = 'https://www.basketball-reference.com/leagues/NBA_2022.html'
+    requests.get(bib_url_2022)
+    data_2022=pd.read_html(bib_url_2022)[5]
+    data2_2022=pd.read_html(bib_url_2022)[10]
+    df = pd.DataFrame(data_2022)
+    data2_2022.columns = data2_2022.columns.droplevel(0)
+    merged_data_2022 = data_2022.merge(data2_2022[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2022['Team'] = merged_data_2022['Team'].str.replace('*', '', regex=False)
+    merged_data_2022
+    merged_data_2022['Year'] = 2022
+    # Drop the last row from merged_data
+    merged_data_2022 = merged_data_2022.drop(merged_data_2022.index[-1])
 
-# SEASON 2021-22
-bib_url_2022 = 'https://www.basketball-reference.com/leagues/NBA_2022.html'
-requests.get(bib_url_2022)
-data_2022=pd.read_html(bib_url_2022)[5]
-data2_2022=pd.read_html(bib_url_2022)[10]
-df = pd.DataFrame(data_2022)
-data2_2022.columns = data2_2022.columns.droplevel(0)
-merged_data_2022 = data_2022.merge(data2_2022[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2022['Team'] = merged_data_2022['Team'].str.replace('*', '', regex=False)
-merged_data_2022
-merged_data_2022['Year'] = 2022
-# Drop the last row from merged_data
-merged_data_2022 = merged_data_2022.drop(merged_data_2022.index[-1])
+    # SEASON 2020-21
+    bib_url_2021 = 'https://www.basketball-reference.com/leagues/NBA_2021.html'
+    requests.get(bib_url_2021)
+    data_2021=pd.read_html(bib_url_2021)[5]
+    data2_2021=pd.read_html(bib_url_2021)[10]
+    df = pd.DataFrame(data_2021)
+    data2_2021.columns = data2_2021.columns.droplevel(0)
+    merged_data_2021 = data_2021.merge(data2_2021[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2021['Team'] = merged_data_2021['Team'].str.replace('*', '', regex=False)
+    merged_data_2021
+    merged_data_2021['Year'] = 2021
+    # Drop the last row from merged_data
+    merged_data_2021 = merged_data_2021.drop(merged_data_2021.index[-1])
 
-# SEASON 2020-21
-bib_url_2021 = 'https://www.basketball-reference.com/leagues/NBA_2021.html'
-requests.get(bib_url_2021)
-data_2021=pd.read_html(bib_url_2021)[5]
-data2_2021=pd.read_html(bib_url_2021)[10]
-df = pd.DataFrame(data_2021)
-data2_2021.columns = data2_2021.columns.droplevel(0)
-merged_data_2021 = data_2021.merge(data2_2021[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2021['Team'] = merged_data_2021['Team'].str.replace('*', '', regex=False)
-merged_data_2021
-merged_data_2021['Year'] = 2021
-# Drop the last row from merged_data
-merged_data_2021 = merged_data_2021.drop(merged_data_2021.index[-1])
+    # SEASON 2019-20
+    bib_url_2020 = 'https://www.basketball-reference.com/leagues/NBA_2020.html'
+    requests.get(bib_url_2020)
+    data_2020=pd.read_html(bib_url_2020)[5]
+    data2_2020=pd.read_html(bib_url_2020)[10]
+    df = pd.DataFrame(data_2020)
+    data2_2020.columns = data2_2020.columns.droplevel(0)
+    merged_data_2020 = data_2020.merge(data2_2020[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2020['Team'] = merged_data_2020['Team'].str.replace('*', '', regex=False)
+    merged_data_2020
+    merged_data_2020['Year'] = 2020
+    # Drop the last row from merged_data
+    merged_data_2020 = merged_data_2020.drop(merged_data_2020.index[-1])
 
-# SEASON 2019-20
-bib_url_2020 = 'https://www.basketball-reference.com/leagues/NBA_2020.html'
-requests.get(bib_url_2020)
-data_2020=pd.read_html(bib_url_2020)[5]
-data2_2020=pd.read_html(bib_url_2020)[10]
-df = pd.DataFrame(data_2020)
-data2_2020.columns = data2_2020.columns.droplevel(0)
-merged_data_2020 = data_2020.merge(data2_2020[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2020['Team'] = merged_data_2020['Team'].str.replace('*', '', regex=False)
-merged_data_2020
-merged_data_2020['Year'] = 2020
-# Drop the last row from merged_data
-merged_data_2020 = merged_data_2020.drop(merged_data_2020.index[-1])
+    # SEASON 2018-19
+    bib_url_2019 = 'https://www.basketball-reference.com/leagues/NBA_2019.html'
+    requests.get(bib_url_2019)
+    data_2019=pd.read_html(bib_url_2019)[5]
+    data2_2019=pd.read_html(bib_url_2019)[10]
+    df = pd.DataFrame(data_2019)
+    data2_2019.columns = data2_2019.columns.droplevel(0)
+    merged_data_2019 = data_2019.merge(data2_2019[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2019['Team'] = merged_data_2019['Team'].str.replace('*', '', regex=False)
+    merged_data_2019
+    merged_data_2019['Year'] = 2019
+    # Drop the last row from merged_data
+    merged_data_2019 = merged_data_2019.drop(merged_data_2019.index[-1])
 
-# SEASON 2018-19
-bib_url_2019 = 'https://www.basketball-reference.com/leagues/NBA_2019.html'
-requests.get(bib_url_2019)
-data_2019=pd.read_html(bib_url_2019)[5]
-data2_2019=pd.read_html(bib_url_2019)[10]
-df = pd.DataFrame(data_2019)
-data2_2019.columns = data2_2019.columns.droplevel(0)
-merged_data_2019 = data_2019.merge(data2_2019[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2019['Team'] = merged_data_2019['Team'].str.replace('*', '', regex=False)
-merged_data_2019
-merged_data_2019['Year'] = 2019
-# Drop the last row from merged_data
-merged_data_2019 = merged_data_2019.drop(merged_data_2019.index[-1])
+    # SEASON 2017-18
+    bib_url_2018 = 'https://www.basketball-reference.com/leagues/NBA_2018.html'
+    requests.get(bib_url_2018)
+    data_2018=pd.read_html(bib_url_2018)[5]
+    data2_2018=pd.read_html(bib_url_2018)[10]
+    df = pd.DataFrame(data_2018)
+    data2_2018.columns = data2_2018.columns.droplevel(0)
+    merged_data_2018 = data_2018.merge(data2_2018[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
+    merged_data_2018['Team'] = merged_data_2018['Team'].str.replace('*', '', regex=False)
+    merged_data_2018
+    merged_data_2018['Year'] = 2018
+    # Drop the last row from merged_data
+    merged_data_2018 = merged_data_2018.drop(merged_data_2018.index[-1])
 
-# SEASON 2017-18
-bib_url_2018 = 'https://www.basketball-reference.com/leagues/NBA_2018.html'
-requests.get(bib_url_2018)
-data_2018=pd.read_html(bib_url_2018)[5]
-data2_2018=pd.read_html(bib_url_2018)[10]
-df = pd.DataFrame(data_2018)
-data2_2018.columns = data2_2018.columns.droplevel(0)
-merged_data_2018 = data_2018.merge(data2_2018[["Team", "Age", "MOV", "Pace", "TS%", "W", "L"]], on='Team', how='left')
-merged_data_2018['Team'] = merged_data_2018['Team'].str.replace('*', '', regex=False)
-merged_data_2018
-merged_data_2018['Year'] = 2018
-# Drop the last row from merged_data
-merged_data_2018 = merged_data_2018.drop(merged_data_2018.index[-1])
-
-# Concatenate all data frames into one
-all_data = pd.concat([merged_data_2023, merged_data_2022, merged_data_2021,
-                     merged_data_2020, merged_data_2019, merged_data_2018])
+    # Concatenate all data frames into one
+    all_data = pd.concat([merged_data_2023, merged_data_2022, merged_data_2021,
+                        merged_data_2020, merged_data_2019, merged_data_2018])
 
 
-all_data.to_csv("data/basketball.csv")
-#run this after editing all_data_1
-all_data_1 = all_data
+    all_data.to_csv("data/basketball.csv")
+    #run this after editing all_data_1
+    return
--- hoopin/visualization.py
+++ hoopin/visualization.py
@@ -7,9 +7,6 @@ import pandas as pd
 import pkg_resources
 import statsmodels
 
-# import hoopin
-# Assuming you have already loaded your data and defined the variables as mentioned in your previous code
-
 path = 'data/basketball.csv'
 
 data_path = pkg_resources.resource_filename('hoopin', path)
@@ -34,26 +31,10 @@ Y_pred = model.predict(X_test)
 
 # Calculate the mean squared error (MSE) to evaluate the model's performance
 mse = mean_squared_error(Y_test, Y_pred)
-print("Mean Squared Error:", mse)
 
 # Inspect the coefficients of the linear regression model to determine variable importance
 coefficients = pd.Series(model.coef_, index=X.columns)
 sorted_coefficients = coefficients.abs().sort_values(ascending=False)
-print("Most important variables:")
-print(sorted_coefficients)
-
-
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-import pandas as pd
-
-
-
-
 
 
 # Visualization 2: Bar chart of Coefficients
@@ -92,30 +73,6 @@ coefficients = coefficients.sort_values(ascending=True)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-def show_barchart():
-    # Visualization 2: Bar chart of Coefficients
-    coefficients = coefficients.sort_values(ascending=True)
-    coefficients = pd.Series(model.coef_, index=X.columns)
-    coefficients = coefficients.sort_values(ascending=True)
-
-    plt.figure(figsize=(10, 6))
-    coefficients.plot(kind='barh')  # Use 'barh' for horizontal bar chart
-    plt.title('Coefficients of the Linear Regression Model')
-    plt.xlabel('Coefficient Value')
-    plt.ylabel('Predictor Variables')
-    plt.show()
-    return
 
 
 
@@ -213,3 +170,37 @@ def show_correlation_heatmap():
     # sorted_coefficients = coefficients.abs().sort_values(ascending=False)
 
     ######
+
+
+
+
+    
+# #ALL COLUMNS
+# #all_columns = ['FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', "MOV", "Pace", "TS%", 'W', 'L']
+
+# # COLUMNS WE ARE KEEPING
+# selected_columns = ['3P%','2P%', 'AST','TRB','STL',"TS%", 'W', 'L']
+
+
+# # Split the data into predictor variables (X) and target variable (Y)
+# X = all_data_1[selected_columns[:-2]]  # All columns except 'W' and 'L'
+# Y = all_data_1['W']
+
+# # Split the data into training and testing sets
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# # Create a linear regression model, fit it to the training data, and make predictions
+# model = LinearRegression()
+# model.fit(X_train, Y_train)
+# Y_pred = model.predict(X_test)
+
+# # Calculate the mean squared error (MSE) to evaluate the model's performance
+# mse = mean_squared_error(Y_test, Y_pred)
+# print("Mean Squared Error:", mse)
+
+# # Inspect the coefficients of the linear regression model to determine variable importance
+# coefficients = pd.Series(model.coef_, index=X.columns)
+# sorted_coefficients = coefficients.abs().sort_values(ascending=False)
+# print("Most important variables:")
+# print(sorted_coefficients)
+
